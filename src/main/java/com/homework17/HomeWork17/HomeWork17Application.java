555e1b1316dcc761da34c8e19802eadb5a62b4cd
MaximShev <max.93@list.ru>
2024-01-29 15:07:40 +0600

    Initial commit

---
+++ src/main/java/com/homework17/HomeWork17/HomeWork17Application.java
@@ -0,0 +1,13 @@
+package com.homework17.HomeWork17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork17Application.class, args);
+	}
+
+}
