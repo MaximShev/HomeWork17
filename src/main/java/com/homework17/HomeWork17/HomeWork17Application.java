d4550caef8a19398b0734cd1988a344c86befd1d
MaximShev <150287175+MaximShev@users.noreply.github.com>
2024-04-02 22:44:10 +0600

    Revert "Tests"

--- src/main/java/com/homework17/HomeWork17/HomeWork17Application.java
+++ src/main/java/com/homework17/HomeWork17/HomeWork17Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomeWork17Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HomeWork17Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork17Application.class, args);
+	}
+
 }
